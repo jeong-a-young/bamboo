b91aaa2672a8ccb1daaac7a4e21413f388a03dd7
ysjkdssj <monayoung0323@naver.com>
2022-11-02 19:52:54 +0900

    Update query.sql

--- Project/Bamboo/WebContent/query.sql
+++ Project/Bamboo/WebContent/query.sql
@@ -8,9 +8,13 @@ CREATE TABLE member (
 CREATE TABLE post (
 	post_id NUMBER (10) PRIMARY KEY,
 	post_writer VARCHAR2 (4),
+	post_title VARCHAR2 (30),
 	post_type VARCHAR2 (20),
 	post_contents VARCHAR (1000)
 );
+
+CREATE SEQUENCE post_seq;
+
 INSERT INTO member VALUES ('admin', '1234', '관리자', 'ys2220205@y-y.hs.kr');
 
 DROP TABLE member;
